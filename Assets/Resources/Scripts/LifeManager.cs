4efb5e68faa86f2c90e31ec846329fa8f6beea9b
tamiwo <tamiwo.otutama@gmail.com>
2019-01-20 14:39:21 +0900

    upate:LifeManager追加

---
+++ Assets/Resources/Scripts/LifeManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeManager : MonoBehaviour
+{
+    public GameObject lifeTextObj;
+    public string key;
+    private Text lifeText;
+    private int life;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeText = lifeTextObj.GetComponent<Text>();
+        Load();
+    }
+
+    public void UpDown(int value)
+    {
+        life += value;
+        lifeText.text = life.ToString();
+        Save();
+    }
+
+    void Save()
+    {
+        PlayerPrefs.SetInt(key + "life", life);
+    }
+
+    void Load()
+    {
+        life = PlayerPrefs.GetInt(key + "life", 20);
+        lifeText.text = life.ToString();
+    }
+
+    void Reset()
+    {
+        life = 20;
+        lifeText.text = life.ToString();
+        Save();
+    }
+}
