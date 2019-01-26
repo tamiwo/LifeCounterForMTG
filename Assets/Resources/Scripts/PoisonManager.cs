330d2e10b8ad23ad243b0947a571cabe35119da8
tamiwo <tamiwo.otutama@gmail.com>
2019-01-27 02:23:25 +0900

    update:PoisonManager作成

---
+++ Assets/Resources/Scripts/PoisonManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonManager : MonoBehaviour
+{
+    public string playerName;
+    public GameObject[] counters;
+
+    const int MAX = 10;
+    const string KEY = "poison";
+
+    int poison;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Load();
+    }
+
+    public void Set( int value )
+    {
+        poison = value;
+
+        for (int i=0; i < MAX; i++)
+        {
+            // value 以下ならAvtiveにする
+            // valueより大きい場合は非Activeにする
+            bool activeFlag = (i < value);
+            counters[i].SetActive(activeFlag);
+        }
+    }
+
+    void Save()
+    {
+        PlayerPrefs.SetInt(playerName + KEY, poison);
+    }
+
+    void Load()
+    {
+        Set(PlayerPrefs.GetInt(playerName + KEY, 0));
+    }
+
+    public void Reset()
+    {
+        Set(0);
+    }
+
+
+}
