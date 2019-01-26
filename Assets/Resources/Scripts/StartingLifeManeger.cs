7039f186dba41567edec5bae6429714f4a8ca599
tamiwo <tamiwo.otutama@gmail.com>
2019-01-26 18:40:27 +0900

    update:StartingLife設定スクリプト作成

---
+++ Assets/Resources/Scripts/StartingLifeManeger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartingLifeManeger : MonoBehaviour
+{
+
+    Text Shape;
+    Text Shadow;
+    private int life;
+    const string key = "StartingLife";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shape = transform.Find("Shape").gameObject.GetComponent<Text>();
+        Shadow = transform.Find("Shadow").gameObject.GetComponent<Text>();
+    }
+
+    public void UpDown(int value)
+    {
+        life += value;
+        Shape.text = life.ToString();
+        Shadow.text = life.ToString();
+        Save();
+    }
+
+    void Save()
+    {
+        PlayerPrefs.SetInt(key, life);
+    }
+
+    void Load()
+    {
+        life = PlayerPrefs.GetInt(key, 20);
+        Shape.text = life.ToString();
+        Shadow.text = life.ToString();
+    }
+
+}
