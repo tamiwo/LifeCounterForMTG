a8ec165545119d60bbdee58c781f7762dec74820
tamiwo <tamiwo.otutama@gmail.com>
2019-01-27 00:14:33 +0900

    update:LifaPanelScaler作成

---
+++ Assets/Resources/Scripts/LifeViewPanelScaler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeViewPanelScaler : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void ChangeScale()
+    {
+        const string name = "isMinimum";
+        bool isMinimum = animator.GetBool(name);
+        animator.SetBool(name, !isMinimum);
+    }
+}
