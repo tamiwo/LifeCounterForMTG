277f9962d61a30add9276f79d74d80da458b40db
tamiwo <tamiwo.otutama@gmail.com>
2020-10-09 19:24:23 +0900

    update:ライフのスクロール

--- Assets/Resources/Scripts/LifePanelManager.cs
+++ Assets/Resources/Scripts/LifePanelManager.cs
@@ -5,9 +5,8 @@ using UnityEngine.UI;
 
 public class LifePanelManager : MonoBehaviour
 {
-    public GameObject lifeTextObj;
+    public ScrollText lifeText;
     public string key;
-    private Text lifeText;
     private int life;
 
     public GameObject startingLifeObj;
@@ -17,14 +16,13 @@ public class LifePanelManager : MonoBehaviour
     void Start()
     {
         startingLife = startingLifeObj.GetComponent<StartingLifeManeger>();
-        lifeText = lifeTextObj.GetComponent<Text>();
         Load();
     }
 
     public void UpDown(int value)
     {
         life += value;
-        lifeText.text = life.ToString();
+        lifeText.targetValue = life;
         Save();
     }
 
@@ -36,19 +34,19 @@ public class LifePanelManager : MonoBehaviour
     public void Load()
     {
         life = PlayerPrefs.GetInt(key + "life", 20);
-        lifeText.text = life.ToString();
+        lifeText.targetValue = life;
     }
 
     public void Reset()
     {
         life = startingLife.Get();
-        lifeText.text = life.ToString();
+        lifeText.targetValue = life;
         Save();
     }
 
     public void DiceRoll()
     {
         // 1〜6の値を表示する
-        lifeText.text = Random.Range(1,7).ToString();
+        lifeText.targetValue = Random.Range(1,7);
     }
 }
---
+++ Assets/Resources/Scripts/ScrollText.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollText : MonoBehaviour
+{
+    private int _currentValue = 0;
+    private int currentValue {
+        set {
+            _currentValue = value;
+            int setVal = value + 2;
+            foreach( Text text in texts){
+                text.text = setVal.ToString();
+                setVal--;
+            }
+        }
+        get {
+            return _currentValue;
+        }
+    }
+    public int targetValue = 0;
+
+    public RectTransform scrollBase;
+    private Vector2 startPos;
+    public Vector2 scrollUnit;
+    public Vector2 speed;
+
+    public List<Text> texts;
+
+    private void Awake() {
+        currentValue = 0;
+        startPos = scrollBase.anchoredPosition;
+    }
+
+    private void Update() {
+
+        int diff = targetValue - currentValue;
+        Debug.Log( targetValue + " : " + currentValue );
+        Vector2 scrollValue;
+
+        if( diff >= 3 ){
+            scrollValue = -speed*Time.deltaTime*3;
+        }
+        else if( diff > 0 ){
+            scrollValue = -speed*Time.deltaTime*diff;
+        }
+        else if( diff < 0 ){
+            scrollValue = -speed*Time.deltaTime*diff;
+        }
+        else if ( diff <= -3 ){
+            scrollValue = speed*Time.deltaTime*3;
+        }
+        else { // (diff == 0)
+            Vector2 diffPos = scrollBase.anchoredPosition-startPos;
+            if( diffPos.y > 10 ){
+                scrollValue = -diffPos;
+            }
+            else if( diffPos.y < -10 ){
+                scrollValue = -diffPos;
+            }
+            else {
+                return;
+            }
+        }
+
+        scrollBase.anchoredPosition += scrollValue;
+        
+        // 一定以上スクロールしたら数値を変える
+        if( scrollBase.anchoredPosition.y > scrollUnit.y ){
+            scrollBase.anchoredPosition -= scrollUnit;
+            currentValue -= 1;
+        }
+        else if( scrollBase.anchoredPosition.y < -scrollUnit.y ){
+            scrollBase.anchoredPosition += scrollUnit;
+            currentValue += 1;
+        }
+    }
+}
