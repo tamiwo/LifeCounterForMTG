903e5317bc561f9c8e62536a326641fe38e5c285
tamiwo <tamiwo.otutama@gmail.com>
2020-11-11 21:47:04 +0900

    add:テキストフェード 用スクリプト

---
+++ Assets/Resources/Scripts/GradientFader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UiEffect;
+
+public class GradientFader : MonoBehaviour
+{
+    public float duration;
+
+    public GradientAlpha gradient;
+    private float alphaDiff;
+    private float timer = 0;
+
+    private void FadeIn() {
+        // 左から右に現れる
+        gradient.alphaTop = 1;
+        gradient.alphaBottom = 1;
+        gradient.alphaLeft = 1;
+        gradient.alphaRight = 0;
+        gradient.gradientOffsetHorizontal = 1;
+        gradient.gradientOffsetVertical = 1;
+        alphaDiff = 2/duration;
+        timer = duration;
+    }
+
+    private void FadeOut() {
+        // 左から右に消える
+        gradient.alphaTop = 1;
+        gradient.alphaBottom = 1;
+        gradient.alphaLeft = 0;
+        gradient.alphaRight = 1;
+        gradient.gradientOffsetHorizontal = 1;
+        gradient.gradientOffsetVertical = 1;
+        alphaDiff = 2/duration;
+        timer = duration;
+    }
+
+    private void OnEnable() {
+        FadeIn();
+    }
+
+    void Update() {
+        if( timer > 0 ) {
+            gradient.gradientOffsetHorizontal -= alphaDiff*Time.deltaTime;
+            timer -= Time.deltaTime;
+        }
+        
+    }
+}
---
+++ Assets/Resources/Scripts/TextFader.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextFader : MonoBehaviour
+{
+    public Vector3 scaleMax;
+    public Vector3 scaleMin;
+    public int fontSizeMax;
+    public int fontSizeMin;
+    public float duration;
+
+    private float time;
+    private Vector3 deltaScale = Vector3.zero;
+    private float deltaFontSize = 0;
+    private float diffFontSize;
+    private float deltaAlpha = 0;
+
+    private Text text = default;
+
+    private void Start() {
+        text = GetComponent<Text>();
+        time = duration;
+    }
+
+    private void OnEnable() {
+        text = GetComponent<Text>();
+        FadeOut();
+    }
+
+    private void Update() {
+        if( time < duration ){
+            time += Time.deltaTime;
+            /*
+            diffFontSize  += (deltaFontSize * Time.deltaTime);
+            Debug.Log(diffFontSize);
+            if( diffFontSize > 1.0f ){
+                text.fontSize += (int)Mathf.Floor(diffFontSize); 
+                diffFontSize -= (int)Mathf.Floor(diffFontSize);
+            }
+            */
+            text.fontSize += (int)(deltaFontSize * Time.deltaTime);
+            if( text.fontSize > fontSizeMax ){
+                text.fontSize = fontSizeMax;
+            }
+            else if( text.fontSize < fontSizeMin ){
+                text.fontSize = fontSizeMin;
+            }
+
+            transform.localScale += deltaScale * Time.deltaTime;
+            
+            Color newColor = text.color;
+            newColor.a += deltaAlpha * Time.deltaTime;
+            text.color = newColor;
+        }
+    }
+
+    public void FadeOut () {
+        text.fontSize = fontSizeMax;
+        transform.localScale = scaleMin;
+        var c = text.color;
+        c.a = 1;
+        text.color = c;
+        deltaScale = ( scaleMax - scaleMin ) / duration;
+        deltaFontSize = -( fontSizeMax - fontSizeMin ) / duration;
+        Debug.Log( deltaFontSize);
+        deltaAlpha = -1/duration;
+        time = 0;
+    }
+
+    public void FadeIn() {
+        deltaScale = -( scaleMax - scaleMin ) / duration;
+        deltaFontSize = ( fontSizeMax - fontSizeMin ) / duration;
+        deltaAlpha = 1/duration;
+        time = 0;
+    }
+}
