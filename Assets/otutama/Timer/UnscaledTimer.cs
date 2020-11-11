d18b852c24bb4980eee7b5067b3a1a76e6206b29
tamiwo <tamiwo.otutama@gmail.com>
2020-11-11 21:55:50 +0900

    add:タイマー

---
+++ Assets/otutama/Timer/UnscaledTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace otutama
+{
+    public class UnscaledTimer : MonoBehaviour
+    {
+        [SerializeField]
+        private float interval = default;
+        [SerializeField]
+        private bool repeat = default;
+        [SerializeField]
+        private UnityEvent events = new UnityEvent();
+        private float time;
+        private void OnEnable()
+        {
+            time = 0;
+        }
+
+        private void Update()
+        {
+            UpdateTimer();
+        }
+            private void UpdateTimer() {
+            time += Time.deltaTime;
+            if( time >= interval){
+                events.Invoke();
+                if( !repeat ){
+                    enabled = false;
+                }
+            }
+        }
+    }
+}
